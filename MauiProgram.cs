80f0f3e27009721a638d56460d37812c29410a42
EmiJalowicki <emilianojalowicki@gmail.com>
2026-08-16 00:58:58 -0300

    feat: agrega NuGet CommunityToolkit.Maui para mejorar detalles visuales

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace PedidoApp
 {
@@ -9,6 +10,7 @@ namespace PedidoApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
